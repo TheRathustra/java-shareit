09a962082a1cdcd75cab6b16c3aab8d1aebadac7
TheRathustra <Purple-Pleb@yandex.ru>
2023-01-04 13:14:48 +0300

    Реализация user service

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -11,7 +11,6 @@ import ru.practicum.shareit.user.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -1,39 +1,15 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import java.util.List;
 
-@Component
-public class UserService {
+public interface UserService {
 
-    private UserStorage userStorage;
-
-    @Autowired
-    public UserService(UserStorage userStorage) {
-        this.userStorage = userStorage;
-    }
-
-    public User add(UserDto userDto) {
-        return userStorage.add(userDto);
-    }
-
-    public User update(long id, UserDto userDto) {
-        return userStorage.update(id, userDto);
-    }
-
-    public void delete(long id) {
-        userStorage.delete(id);
-    }
-
-    public User getUserById(long id) {
-        return userStorage.getUserById(id);
-    }
-
-    public List<User> getUsers() {
-        return userStorage.getUsers();
-    }
+    User add(UserDto userDto);
+    User update(long id, UserDto userDto);
+    void delete(long id);
+    User getUserById(long id);
+    List<User> getUsers();
 
 }
