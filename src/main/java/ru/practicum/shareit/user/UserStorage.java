515bd5a6c97e9c277f96a53a174747dd32165a46
TheRathustra <Purple-Pleb@yandex.ru>
2023-01-04 13:24:48 +0300

    рефакторинг

--- src/main/java/ru/practicum/shareit/user/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/UserStorage.java
@@ -7,8 +7,12 @@ import java.util.List;
 public interface UserStorage {
 
     User add(UserDto userDto);
+
     User update(long id, UserDto userDto);
+
     void delete(long id);
+
     User getUserById(long id);
+
     List<User> getUsers();
 }
