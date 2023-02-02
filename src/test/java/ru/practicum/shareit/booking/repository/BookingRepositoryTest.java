66a35f9adc09979ee04341575869a0baf6e31841
TheRathustra <Purple-Pleb@yandex.ru>
2023-02-02 22:32:53 +0300

    Добавил тесты

---
+++ src/test/java/ru/practicum/shareit/booking/repository/BookingRepositoryTest.java
@@ -0,0 +1,14 @@
+package ru.practicum.shareit.booking.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class BookingRepositoryTest {
+
+    @Test
+    void repositoryTest() {
+
+    }
+
+}
