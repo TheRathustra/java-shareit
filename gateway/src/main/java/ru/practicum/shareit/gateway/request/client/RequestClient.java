592b04ee8b9c9809ef79f4acbfdb972729c81685
TheRathustra <Purple-Pleb@yandex.ru>
2023-02-06 01:37:42 +0300

    feature: add-docker - исправления код-стайл

--- gateway/src/main/java/ru/practicum/shareit/gateway/request/client/RequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/request/client/RequestClient.java
@@ -43,6 +43,7 @@ public class RequestClient extends BaseClient {
 
         return get("/all?from={from}&size={size}", userId, parameters);
     }
+
     public ResponseEntity<Object> getItemRequestById(Long userId, Long requestId) {
         return get("/" + requestId, userId);
     }
